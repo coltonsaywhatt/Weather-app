77a47856730434333eb54b24cf27ccd4cf7a1734
coltonsaywhatt <coltonsaywhatt@gmail.com>
2022-09-04 03:01:22 -0400

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  weatherApiBaseUrl: 'https://rapidweather.p.rapidapi.com/data/2.5/weather',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'rapidweather.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: 'b4d5934303msh9690db10ccf5b55p1a66a1jsn5c6757d1bf39'
 };
 
 /*
